2f7f447cd2e16a140c6e2f7d9ffd37f01eea269d
Evgenii <osipenkov.evgenii@gmail.com>
2023-11-24 10:11:27 +0300

    Recreate useCookie bug

---
+++ components/BaseFoo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Component {{ id }}</h2>
+
+    <button @click="setCookie">Set cookie to {{ id }}</button>
+    <button @click="resetCookie">Reset cookie</button>
+
+    <p>{{ `${foo}` }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { useCookie } from '#imports'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+
+const { id } = toRefs(props)
+
+const foo = useCookie<string | undefined>('foo')
+
+const setCookie = () => (foo.value = id.value)
+const resetCookie = () => (foo.value = undefined)
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Bug with useCookie</h1>
+
+    <base-foo id="1" key="1" />
+
+    <base-foo id="2" key="2" />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineNuxtComponent } from '#imports'
+import BaseFoo from '~/components/BaseFoo.vue'
 
-export default Vue.extend({
-  name: 'IndexPage',
+export default defineNuxtComponent({
+  name: 'PageCookies',
+
+  components: {
+    BaseFoo,
+  },
 })
 </script>
